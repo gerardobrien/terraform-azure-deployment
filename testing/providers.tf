47b5804c88e160679631ef7f1ffd08dafc7d5aff
Gerard O'Brien <obrien.g@gmail.com>
2023-07-01 16:31:03 +0100

    initial commit

---
+++ testing/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.62.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+}
